440910c0eae67e0936df1a8889023b32579bf050
marie22 <marrienast@gmail.com>
2016-02-03 11:37:52 -0800

    FirstCommit

---
+++ Task2(SoftTheme)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_SoftTheme_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input a number");
+                int num = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.Write(" " + i);
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
